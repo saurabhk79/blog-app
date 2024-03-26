08bc9884ff327ca493100ce40ffd33fdfe7e6868
Sauby <imsauby@gmail.com>
2024-03-26 21:40:18 +0530

    styled home hero

--- frontend/src/components/Hero/index.jsx
+++ frontend/src/components/Hero/index.jsx
@@ -3,7 +3,7 @@ import styles from "./hero.module.css";
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <h1>Blogish</h1>
+      <h1>~Blogish~</h1>
       <h3>Best Blogging Platform you would ever see!</h3>
     </section>
   );
