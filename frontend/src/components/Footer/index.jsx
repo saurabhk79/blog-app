778ab79a7b665fa134dd1950060465a85c0cab18
Sauby <imsauby@gmail.com>
2024-03-26 21:43:33 +0530

    styled home footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -2,9 +2,8 @@ import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>Blogish - Best Blogging Platform you would ever see!</h1>
-      <hr />
+    <footer className={styles.footer}>
+      <h3>Blogish - Best Blogging Platform you would ever see!</h3>
       <p className={styles.tagline}>Made with love by Saurabh!</p>
     </footer>
   );
