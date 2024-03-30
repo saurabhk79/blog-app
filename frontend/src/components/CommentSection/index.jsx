ef23f7e9225e40ec3dc0d5ca0df3a83e497327a3
Sauby <imsauby@gmail.com>
2024-03-30 19:27:55 +0530

    styled and removed bugs from comment

--- frontend/src/components/CommentSection/index.jsx
+++ frontend/src/components/CommentSection/index.jsx
@@ -1,35 +1,58 @@
 import { useNavigate, useParams } from "react-router-dom";
 import Comment from "../Comment";
 import styles from "./commentsection.module.css";
-import { useGetCommentsQuery } from "../../services/blog";
+import {
+  useAddCommentMutation,
+  useGetCommentsQuery,
+} from "../../services/blog";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const CommentSection = () => {
   const params = useParams();
-  const { data, isLoading } = useGetCommentsQuery(params.blogId);
+  const { data, isLoading, refetch } = useGetCommentsQuery(params.blogId);
+  // eslint-disable-next-line no-unused-vars
+  const [addComment, result] = useAddCommentMutation();
+  const [text, setText] = useState("");
 
   const navigate = useNavigate();
   const isLoggedIn = useSelector((state) => state.auth.isLogged);
+
+  const handleAddComment = (e) => {
+    e.preventDefault();
+
+    addComment({ id: params.blogId, comment: text });
+    refetch();
+  };
 
   useEffect(() => {
     if (!isLoggedIn) {
       navigate("/login");
       return;
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoggedIn]);
   if (isLoading) return <h1>Loading...</h1>;
 
   console.log(data);
   return (
     <div className={styles.commentsection}>
+      <div className={styles.back} onClick={() => navigate(-1)}>
+        <h2>{"<-"}</h2>
+      </div>
       <h1>Comments</h1>
 
-      <form className={styles.form}>
-        <input type="text" placeholder="Type your comment..." />
+      <form className={styles.form} onSubmit={handleAddComment}>
+        <input
+          type="text"
+          placeholder="Type your comment..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
 
-        <button>Submit</button>
+        <button type="submit" disabled={text.length === 0}>
+          Submit
+        </button>
       </form>
 
       {data.map((val) => {
